615eb7564c6be6e5cc193914e920fc2562cfa660
Jeffrey Chan <jeffrey2212@gmail.com>
2024-05-06 11:27:54 +0800

    refactor: Update TrajectoryFlowMap layer to handle long input type

--- model/layers/tfm.py
+++ model/layers/tfm.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
 
+
 class TrajectoryFlowMap(nn.Module):
     def __init__(self, num_pois, embedding_dim):
         super(TrajectoryFlowMap, self).__init__()
         self.embedding = nn.Embedding(num_pois, embedding_dim)
-        self.transition_matrix = nn.Parameter(torch.zeros(num_pois, num_pois))
 
     def forward(self, x):
+        x = x.long()  # Ensure input to embedding is of type long
         poi_embeddings = self.embedding(x)
         tfm_matrix = torch.softmax(self.transition_matrix, dim=1)
         poi_embeddings_tfm = torch.matmul(tfm_matrix, poi_embeddings)
